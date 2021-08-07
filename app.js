def2bee3f9f94d20864503cc001e6662ec379875
Dmitrii <dpushk@gmail.com>
2021-08-07 14:56:57 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,34 @@ const upBtn = document.querySelector ('.up-button')
 const downBtn = document.querySelector ('.down-button')
 const sidebar = document.querySelector ('.sidebar')
 const mainSlide = document.querySelector ('.main-slide')
-const slideCount = mainSlide.querySelectorAll ('div').length
+const slidesCount = mainSlide.querySelectorAll ('div').length
+const container = document.querySelector ('.container')
+const height = container.clientHeight
+let activeSlideIndex = 0
 
+sidebar.style.top = `-${(slidesCount - 1)* 100}vh`
 
-sidebar.style.top = `-${ * 100}vh`
+upBtn.addEventListener ('click', () => {
+    changeSlide ('up')
+})
+
+downBtn.addEventListener ('click', () => {
+    changeSlide ('down')
+})
+
+function changeSlide (direction) {
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex === slidesCount) {
+            activeSlideIndex = 0
+        }
+    } else if (direction === 'down') {
+        activeSlideIndex--
+        if (activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1
+        }
+    }
+
+    mainSlide.style.transform = 'translateY (-${activeSlideIndex * height}px)'
+
+}
